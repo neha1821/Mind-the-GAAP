6c83efc875399255cc06702819cb002702d17145
MGSCHN-SURBHI KUMARI <MGSCHN-SURBHI KUMARI@MGSCHN-LAP-351>
2023-11-03 19:45:09 +0530

    created cheque domain class by surbhi

---
+++ Mind_the_Gaap/src/main/java/com/mindgate/main/domain/Cheque.java
@@ -0,0 +1,10 @@
+package com.mindgate.main.domain;
+
+public class Cheque {
+	private int chequeId;
+	private String chequeDate;
+	private double amount;
+	private int senderAccountId;
+	private int receiverAccountId;
+
+}
