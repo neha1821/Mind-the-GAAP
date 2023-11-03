a616f9d5ec83a0d5d3ed5d7c0a80e7c531761ec0
neha1821 <148180103+neha1821@users.noreply.github.com>
2023-11-03 19:19:16 +0530

    Add files via upload

---
+++ Mind_the_Gaap/src/main/java/com/mindgate/main/MindTheGaapApplication.java
@@ -0,0 +1,13 @@
+package com.mindgate.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindTheGaapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindTheGaapApplication.class, args);
+	}
+
+}
