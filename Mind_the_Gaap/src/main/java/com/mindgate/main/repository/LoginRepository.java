5bfc4fd911c2fa7adf46e9fb196dd43fa84b6fb9
MGSCHN-NEHA KUMARI <MGSCHN-NEHA KUMARI@MGSCHN-LAP-353>
2023-11-07 17:12:50 +0530

    Merge branch 'main' of https://github.com/neha1821/Mind-the-GAAP.git into main
    login table created by neha

--- Mind_the_Gaap/src/main/java/com/mindgate/main/repository/LoginRepository.java
+++ Mind_the_Gaap/src/main/java/com/mindgate/main/repository/LoginRepository.java
@@ -14,41 +14,53 @@ public class LoginRepository implements LoginRepositoryInterface {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	private final static String INSERT_NEW_LOGIN = "insert into login_details values(login_id_sequence.nextVal,?,?,?,?)";
-	private final static String UPDATE_EXISTING_LOGIN = "update login_details set password = ?,count=?, typeOfMember=?, loginStatus = ? where loginId = ?";
-	private final static String DELETE_EXISTING_LOGIN = "delete from login_details where login_Id=?";
+	private final static String INSERT_NEW_LOGIN = "insert into login_details values(login_id_sequence.nextVal,?,?,?,?,?)";
+	private final static String UPDATE_EXISTING_LOGIN = "update login_details set password = ?,count=?, type_of_member=?, login_status = ? where login_id = ?";
+	private final static String DELETE_EXISTING_LOGIN = "delete from login_details where login_id=?";
 	private final static String SELECT_ALL_LOGINS = "select * from login_details";
-	private final static String SELECT_ONE_LOGIN = " select * from login_details where login_Id = ?";
+	private final static String SELECT_ONE_LOGIN = " select * from login_details where login_id = ?";
 
 	
 	@Override
 	public boolean addNewLogin(LoginDetails loginDetails) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		Object[] parameters = { loginDetails.getCustomerId(),loginDetails.getPassword(),loginDetails.getCount(),loginDetails.getTypeOfMember(),loginDetails.getLoginStatus()};
+		int rowCount = jdbcTemplate.update(INSERT_NEW_LOGIN, parameters);
+		if (rowCount > 0)
+			return true;
+		else
+			return false;	}
 
 	@Override
 	public LoginDetails updateLogin(LoginDetails loginDetails) {
-		// TODO Auto-generated method stub
+		Object[] parameters = {loginDetails.getLoginId(),loginDetails.getCustomerId(),loginDetails.getPassword(),loginDetails.getCount(),loginDetails.getTypeOfMember(),loginDetails.getLoginStatus() };
+		int rowCount = jdbcTemplate.update(UPDATE_EXISTING_LOGIN, parameters);
+		if (rowCount > 0) {
+			return getLoginByLoginId(loginDetails.getLoginId());
+		}
 		return null;
 	}
 
 	@Override
 	public boolean deleteLogin(int loginId) {
-		// TODO Auto-generated method stub
-		return false;
+		int rowCount = jdbcTemplate.update(DELETE_EXISTING_LOGIN, loginId);
+		if (rowCount > 0)
+			return true;
+		else
+			return false;
 	}
 
 	@Override
 	public LoginDetails getLoginByLoginId(int loginId) {
-		// TODO Auto-generated method stub
-		return null;
+		LoginRowMapper loginRowMapper = new LoginRowMapper();
+		return jdbcTemplate.queryForObject(SELECT_ONE_LOGIN,loginRowMapper, loginId);
+
 	}
 
 	@Override
 	public List<LoginDetails> getAllLogins() {
-		// TODO Auto-generated method stub
-		return null;
+		LoginRowMapper loginRowMapper = new LoginRowMapper();
+		return jdbcTemplate.query(SELECT_ALL_LOGINS, loginRowMapper);
+
 	}
 
 }
