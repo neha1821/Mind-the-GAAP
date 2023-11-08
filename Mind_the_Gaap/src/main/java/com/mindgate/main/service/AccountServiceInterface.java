c6fc50442d002b3096a9c450a2f3297bb3f85e1f
MGSCHN-RAJESH R <MGSCHN-RAJESH R@MGSCHN-LAP-343>
2023-11-08 17:51:46 +0530

    Commiting Transaction

--- Mind_the_Gaap/src/main/java/com/mindgate/main/service/AccountServiceInterface.java
+++ Mind_the_Gaap/src/main/java/com/mindgate/main/service/AccountServiceInterface.java
@@ -7,8 +7,12 @@ import com.mindgate.main.domain.Account;
 public interface AccountServiceInterface {
 
 	boolean addNewAccount(Account account);
+
 	Account updateAccount(Account account);
+
 	boolean deleteAccount(int accountId);
+
 	Account getAccountByAccountId(int accountId);
+
 	List<Account> getAllAccount();
 }
