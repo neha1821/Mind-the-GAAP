3076edd2e86404100a948ae59dae97f7fb89c487
MGSCHN-SURBHI KUMARI <MGSCHN-SURBHI KUMARI@MGSCHN-LAP-351>
2023-11-08 18:08:24 +0530

    initial commit

--- Mind_the_Gaap/src/main/java/com/mindgate/main/repository/ChequeRepository.java
+++ Mind_the_Gaap/src/main/java/com/mindgate/main/repository/ChequeRepository.java
@@ -10,17 +10,16 @@ import com.mindgate.main.domain.Cheque;
 
 @Repository
 public class ChequeRepository implements ChequeRepositoryInterface {
-	
+
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	private final static String INSERT_NEW_CHEQUE = "insert into cheque values(cheque_id_sequence.nextVal,?,?,?,?,?,?)";
-	private final static String UPDATE_EXISTING_CHEQUE = "update cheque set issue_cheque_date = ?, amount = ?, sender_account_id = ?, receiver_account_id = ?, clearance_cheque_date = ?, cheque_status = ?  where cheque_id = ?";
+	private final static String UPDATE_EXISTING_CHEQUE = "update cheque set ISSUE_CHEQUE_DATE=?,AMOUNT =?,ACCOUNT_ID=?,RECEIVER_ACCOUNT_ID=?,CLEARANCE_CHEQUE_DATE=?,CHEQUE_STATUS=?  where CHEQUE_ID = ?";
 	private final static String DELETE_EXISTING_CHEQUE = "delete from cheque where cheque_id=?";
 	private final static String SELECT_ALL_CHEQUES = "select * from CHEQUE ch,ACCOUNT_DETAILS a,CUSTOMER_DETAILS c Where c.CUSTOMER_ID=a.CUSTOMER_ID and ch.ACCOUNT_ID=a.ACCOUNT_ID";
 	private final static String SELECT_ONE_CHEQUE = "select * from CHEQUE ch,ACCOUNT_DETAILS a,CUSTOMER_DETAILS c Where c.CUSTOMER_ID=a.CUSTOMER_ID and ch.ACCOUNT_ID=a.ACCOUNT_ID and CHEQUE_ID=?";
 
-	
 	@Override
 	public List<Cheque> getAllCheque() {
 		ChequeRowMapper chequeRowMapper = new ChequeRowMapper();
@@ -29,7 +28,9 @@ public class ChequeRepository implements ChequeRepositoryInterface {
 
 	@Override
 	public boolean addNewCheque(Cheque cheque) {
-		Object[] parameters = { cheque.getChequeDate(),cheque.getAmount(),cheque.getAccount().getAccountId(),cheque.getReceiverAccountId().getAccountId(),cheque.getClearanceChequeDate(),cheque.getChequeStatus()};
+		Object[] parameters = { cheque.getChequeDate(), cheque.getAmount(), cheque.getAccount().getAccountId(),
+				cheque.getReceiverAccountId().getAccountId(), cheque.getClearanceChequeDate(),
+				cheque.getChequeStatus() };
 		int rowCount = jdbcTemplate.update(INSERT_NEW_CHEQUE, parameters);
 		if (rowCount > 0)
 			return true;
@@ -39,11 +40,13 @@ public class ChequeRepository implements ChequeRepositoryInterface {
 
 	@Override
 	public Cheque updateCheque(Cheque cheque) {
-		Object[] parameters = { cheque.getChequeDate(),cheque.getAmount(),cheque.getAccount().getAccountId(),cheque.getReceiverAccountId().getAccountId(),cheque.getClearanceChequeDate(),cheque.getChequeStatus(),cheque.getChequeId()};
+		Object[] parameters = { cheque.getChequeDate(), cheque.getAmount(), cheque.getAccount().getAccountId(),
+				cheque.getReceiverAccountId().getAccountId(), cheque.getClearanceChequeDate(), cheque.getChequeStatus(),
+				cheque.getChequeId() };
 		int rowCount = jdbcTemplate.update(UPDATE_EXISTING_CHEQUE, parameters);
 		if (rowCount > 0) {
 			return getChequeByChequeId(cheque.getChequeId());
-			}
+		}
 		return null;
 	}
 
@@ -59,9 +62,7 @@ public class ChequeRepository implements ChequeRepositoryInterface {
 	@Override
 	public Cheque getChequeByChequeId(int chequeId) {
 		ChequeRowMapper chequeRowMapper = new ChequeRowMapper();
-		 return jdbcTemplate.queryForObject(SELECT_ONE_CHEQUE,chequeRowMapper, chequeId);
+		return jdbcTemplate.queryForObject(SELECT_ONE_CHEQUE, chequeRowMapper, chequeId);
 	}
-
-	
 
 }
