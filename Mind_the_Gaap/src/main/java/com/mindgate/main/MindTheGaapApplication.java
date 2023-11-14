f24f8e5d842faae7ca2b1b30f0ceeedc57554118
MGSCHN-NEHA KUMARI <MGSCHN-NEHA KUMARI@MGSCHN-LAP-353>
2023-11-14 15:51:50 +0530

    hi

--- Mind_the_Gaap/src/main/java/com/mindgate/main/MindTheGaapApplication.java
+++ Mind_the_Gaap/src/main/java/com/mindgate/main/MindTheGaapApplication.java
@@ -9,5 +9,6 @@ public class MindTheGaapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MindTheGaapApplication.class, args);
 		System.out.println("Main Application");
+		System.out.println("Commited.....");
 	}
 }
